7e06db3945168bb79f84dcb622ccda7a34e7f1fc
beembarik <117326541+beembarik@users.noreply.github.com>
2025-08-12 13:01:49 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import './InstallButton.js'
+
+
 
 createRoot(document.getElementById("root")!).render(<App />);
